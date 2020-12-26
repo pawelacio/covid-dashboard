4b8c12619de9a450b215d80deb39b0fcef31ead7
Paweł Kaczmarek <pawel.kaczmarek95@gmail.com>
2020-12-26 19:55:50 +0100

    Update switch position on mobile devices v2

--- src/components/Header/HeaderStyled.js
+++ src/components/Header/HeaderStyled.js
@@ -1,13 +1,18 @@
 import styled from 'styled-components'
 import themesStyles from '../../styles/themes';
+import { breakpoints, responsiveFrom } from '../../helpers/breakpoints';
 
 export const HeaderStyled = styled.header`
   height: 40px;
-  padding: 10px 60px;
+  padding: 10px 20px;
   display: flex;
   justify-content: space-between;
   align-items: center;
   background-color: ${ props => props.theme ? themesStyles[props.theme].cardColor : 'blue' };
+
+  ${responsiveFrom(breakpoints.medium)`
+    padding: 10px 60px;
+  `}
 `;
 
 export const LogoStyled = styled.img`
